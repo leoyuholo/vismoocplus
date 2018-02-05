ec9d1d29597fbfd21cf9dfad5ac6ef5138c22073
Leo Yu Ho, Lo <leoyuholo@gmail.com>
2018-02-05 21:39:56 +0800

    fix filter criteria

--- front/src/store/getters.js
+++ front/src/store/getters.js
@@ -3,7 +3,7 @@ import { compareDate } from '../helpers'
 export default {
   lectureId: state => state.route.params.lectureId,
   courseId: state => state.route.params.courseId,
-  lectures: state => state.lectures.filter(l => l.releaseDate && compareDate(l.releaseDate, Date.now()) <= 0),
+  lectures: state => state.lectures.filter(l => !l.releaseDate || compareDate(l.releaseDate, Date.now()) <= 0),
   lecture: (state, getters) => state.lectures.find(l => l.objectId === getters.lectureId),
   userAdmin: state => state.user.get('admin'),
   lectureProgress: state => lectureId => state.userSetting ? state.userSetting['progress_' + lectureId] || 0 : 0
