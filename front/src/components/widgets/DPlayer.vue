f929fdf5504ad31f9319c4d2f88e59cd7ce41c96
Leo Yu Ho, Lo <leoyuholo@gmail.com>
2018-03-23 16:34:05 +0800

    fix hotkey

--- front/src/components/widgets/DPlayer.vue
+++ front/src/components/widgets/DPlayer.vue
@@ -9,44 +9,46 @@ import 'dplayer/dist/DPlayer.min.css'
 class HotKey {
   constructor (player) {
     this.eventListener = (e) => {
-      const tag = document.activeElement.tagName.toUpperCase()
-      const editable = document.activeElement.getAttribute('contenteditable')
-      if (tag !== 'INPUT' && tag !== 'TEXTAREA' && editable !== '' && editable !== 'true') {
-        const event = e || window.event
-        let percentage
+      if (player.focus) {
+        const tag = document.activeElement.tagName.toUpperCase()
+        const editable = document.activeElement.getAttribute('contenteditable')
+        if (tag !== 'INPUT' && tag !== 'TEXTAREA' && editable !== '' && editable !== 'true') {
+          const event = e || window.event
+          let percentage
+          switch (event.keyCode) {
+            case 32:
+              event.preventDefault()
+              player.toggle()
+              break
+            case 37:
+              event.preventDefault()
+              player.seek(player.video.currentTime - 5)
+              player.controller.setAutoHide()
+              break
+            case 39:
+              event.preventDefault()
+              player.seek(player.video.currentTime + 5)
+              player.controller.setAutoHide()
+              break
+            case 38:
+              event.preventDefault()
+              percentage = player.volume() + 0.1
+              player.volume(percentage)
+              break
+            case 40:
+              event.preventDefault()
+              percentage = player.volume() - 0.1
+              player.volume(percentage)
+              break
+          }
+        }
         switch (event.keyCode) {
-          case 32:
-            event.preventDefault()
-            player.toggle()
-            break
-          case 37:
-            event.preventDefault()
-            player.seek(player.video.currentTime - 5)
-            player.controller.setAutoHide()
-            break
-          case 39:
-            event.preventDefault()
-            player.seek(player.video.currentTime + 5)
-            player.controller.setAutoHide()
-            break
-          case 38:
-            event.preventDefault()
-            percentage = player.volume() + 0.1
-            player.volume(percentage)
-            break
-          case 40:
-            event.preventDefault()
-            percentage = player.volume() - 0.1
-            player.volume(percentage)
+          case 27:
+            if (player.fullScreen.isFullScreen('web')) {
+              player.fullScreen.cancel('web')
+            }
             break
         }
-      }
-      switch (event.keyCode) {
-        case 27:
-          if (player.fullScreen.isFullScreen('web')) {
-            player.fullScreen.cancel('web')
-          }
-          break
       }
     }
     document.addEventListener('keydown', this.eventListener)
